1380d0a15b6881c3bd3f1f265f174f909e8b5d8b
Sahak <sahak79@hotmail.com>
2016-04-05 00:08:34 +0400

    improve xQuery

--- src/xquery.js
+++ src/xquery.js
@@ -1,36 +1,48 @@
 var __xQuery__ = (function() {
-    var selectedElement;
-    var selectedElements;
+
+    var selectedElements = [];
+
 
     function select(selector) {
-        selectedElement = undefined;
-        selectedElements = undefined;
         if(selector.substr(0, 1) === ".") {
-            selectedElements = document.getElementsByClassName(selector.substr(1, selector.length - 1));
+            selectedElements.concat(document.getElementsByClassName(selector.substr(1, selector.length - 1)));
         }else if(selector.substr(0, 1) === "#") {
-            selectedElement = document.getElementById(selector.substr(1, selector.length - 1));
+            selectedElements.push(document.getElementById(selector.substr(1, selector.length - 1)));
         }else if(/^[a-zA-Z]+$/.test(selector)) {
-            selectedElements = document.getElementsByTagName(selector);
-        }else {
-            throw "No such selector ["+selector+"]";
+            selectedElements.concat(document.getElementsByTagName(selector));
         }
         return this;
     }
 
-    /** attach styles for element/elements  */
-    function css(param1, param2) {
-        var style = {};
-        var styleContainer = [];
-        if(param1 !== undefined && param2 !== undefined) {
-            style.name = param1;
-            style.value = param2;
-            styleContainer[0] = style;
-            attachStyles(styleContainer);
-        }else if(param1 !== undefined && typeof param1 === 'object' && param2 === undefined) {
-            attachMultipleStyles(param1);
-        }else {
-            throw "Provided style can't be applied";
+    /**  attach styles for element/elements  */
+    function css() {
+
+        var stylesArray = [];
+        var styleItem;
+
+        if(arguments.length == 1) {
+            for (var key in arguments[0]) {
+                if(arguments[0].hasOwnProperty(key)) {
+                    styleItem = {name : key, value : arguments[0][key]};
+                    stylesArray.push(styleItem);
+                }
+            }
+        }else if(arguments.length == 2) {
+            styleItem = {name : arguments[0], value : arguments[1]};
+            stylesArray.push(styleItem);
         }
+
+
+//        var style = {};
+//        var styleContainer = [];
+//        if(param1 !== undefined && param2 !== undefined) {
+//            style.name = param1;
+//            style.value = param2;
+//            styleContainer[0] = style;
+//            attachStyles(styleContainer);
+//        }else if(param1 !== undefined && typeof param1 === 'object' && param2 === undefined) {
+//            attachMultipleStyles(param1);
+//        }
         return this;
     }
 
@@ -64,6 +76,31 @@ var __xQuery__ = (function() {
             element.style[styles[j].name] = styles[j].value;
         }
     }
+
+    // ----------------
+    function myMove() {
+        var elem = document.getElementById("animate");
+        var pos = 0;
+        var id = setInterval(frame, 5);
+        function frame() {
+            if (pos == 350) {
+                clearInterval(id);
+            } else {
+                pos++;
+                elem.style.top = pos + 'px';
+                elem.style.left = pos + 'px';
+            }
+        }
+    }
+
+    function hide() {
+        var opacity0 = 0;
+        var caller = setInterval();
+    }
+
+
+
+
 
     return{
         select: select,
